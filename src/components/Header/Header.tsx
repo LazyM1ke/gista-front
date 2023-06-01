788c50ca95621a91cd50546de954cf1bc991bd71
Ivanov Mikhail <mishaivanov530@gmail.com>
2023-06-01 17:32:39 +0200

    Фиксы верстки + добавил адаптив страницы авторизации

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,26 @@ import Logo from "../../UIkit/Logo/Logo";
 import Typography from "../../UIkit/Typography";
 import "./Header.scss";
 import HeaderProps from "./HeaderProps.types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Header = ({ children }: HeaderProps) => {
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+  }, [screenWidth]);
+
+  if (screenWidth <= 550) {
+    return (
+      <header className="header">
+        <div className="header__logo">
+          <Logo />
+          <Typography variant="title-h1" color="#007FFF">
+            Сдать Гисту
+          </Typography>
+        </div>
+      </header>
+    );
+  }
   return (
     <header className="header">
       <div className="header__logo">
--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -2,13 +2,62 @@ import Button from "../../UIkit/Button";
 import TextInput from "../../UIkit/Input/TextInput/TextInput";
 import Typography from "../../UIkit/Typography";
 import MacImage from "../../assets/images/imac.png";
-import Header from "../../components/Header/Header";
 import "./AuthPage.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AuthPage = () => {
-  const [email, setEmail] = useState<string>("test");
+  const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+  }, [screenWidth]);
+
+  if (screenWidth <= 550) {
+    return (
+      <div className="auth-page">
+        <div className="auth-page__content">
+          <form className="auth-page__form">
+            <Typography
+              className="auth-page__form__title"
+              variant="headline-h2"
+            >
+              Вход
+            </Typography>
+            <div className="auth-page__form__inputs">
+              <TextInput
+                type="text"
+                className="auth-page__form__input"
+                value={email}
+                setValue={setEmail}
+                placeholder="E-mail"
+              />
+              <TextInput
+                type="password"
+                className="auth-page__form__input"
+                value={password}
+                setValue={setPassword}
+                placeholder="Пароль"
+              />
+            </div>
+            <Typography variant="text-14" color="#007FFF">
+              Забыли пароль?
+            </Typography>
+          </form>
+          <div className="auth-page__form__submit">
+            <Button fullWidth type="invert">
+              Войти
+            </Button>
+            <div className="auth-page__form__register">
+              <Typography color="#FFFFFF">Нет аккаунта?</Typography>
+              <Button type="invert">Зарегистрироваться</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="auth-page">
       <div className="auth-page__title">
