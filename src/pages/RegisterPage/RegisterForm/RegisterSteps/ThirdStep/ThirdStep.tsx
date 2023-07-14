55f978e3ab96cedb62ae0136f3e8b4e45e8a72bb
Ivanov Mikhail <mishaivanov530@gmail.com>
2023-07-15 00:11:07 +0200

    сделал инпут кода подтверждения

--- src/pages/RegisterPage/RegisterForm/RegisterSteps/ThirdStep/ThirdStep.tsx
+++ src/pages/RegisterPage/RegisterForm/RegisterSteps/ThirdStep/ThirdStep.tsx
@@ -1,11 +1,12 @@
-import NumberInput from "../../../../../UIkit/Input/NumberInput/NumberInput";
 import Typography from "../../../../../UIkit/Typography";
 import "./ThirdStep.scss";
 import React, { useState } from "react";
 import StepProps from "../StepProsp.types";
 import Button from "../../../../../UIkit/Button";
+import AuthCode from "react-auth-code-input";
 
 const ThirdStep = ({setStep}: StepProps) => {
+    const [code, setCode] = useState<string>('');
   return (
     <div className="third-step">
       <div className="third-step__title">
@@ -15,13 +16,7 @@ const ThirdStep = ({setStep}: StepProps) => {
         </Typography>
       </div>
       <div className="third-step__inputs">
-        {/*инпуты нужно будет доделать*/}
-        <NumberInput />
-        <NumberInput />
-        <NumberInput />
-        <NumberInput />
-        <NumberInput />
-        <NumberInput />
+          <AuthCode containerClassName='code-container' inputClassName='code' allowedCharacters='numeric' onChange={res => setCode(res)} />
       </div>
         <Button size="large" fullWidth onClick={() => setStep(4)}>
             Продолжить
