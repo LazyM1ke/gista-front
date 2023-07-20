f90ee41c37d6c9c8ac64a538f8fb34e63fec7403
Ivanov Mikhail <mishaivanov530@gmail.com>
2023-07-20 19:59:32 +0200

    Подключил редакс и настроил базовый стор

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import {Provider} from "react-redux";
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RecoilRoot>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </Provider>
   </RecoilRoot>
 );
